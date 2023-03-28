186133b130f44860217a6cdef0e4207493a5cce0
Ha Nguyen <habom2310@gmail.com>
2023-03-28 16:00:12 +1100

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,17 +92,10 @@ if 'WEBSITE_HOSTNAME' in os.environ:
     }
 else:
     DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DBNAME', ''),
-        'HOST': os.environ.get('DBHOST', ''),
-        'USER': os.environ.get('DBUSER', ''),
-        'PASSWORD': os.environ.get('DBPASS', ''),
-    } 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
