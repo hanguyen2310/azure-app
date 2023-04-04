dc6a7f39ccf3a2b0f42863b51aa64d75f42a68b3
Ha Nguyen <habom2310@gmail.com>
2023-04-04 23:13:38 +1000

    update

--- pricecheck/scraper.py
+++ pricecheck/scraper.py
@@ -11,6 +11,12 @@ import platform
 # options = webdriver.FirefoxOptions()
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
+options.add_argument("start-maximized")
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 # check os if windows or linux
 
 if platform.system() == "Windows":
